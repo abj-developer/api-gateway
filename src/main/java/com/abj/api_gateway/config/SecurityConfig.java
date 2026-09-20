ff84d9139c3e2ab047219bf5e5d0e7c69c663ccb
ambuj <srivastavvaambuj42@gmail.com>
2026-09-20 11:56:40 +0530

    adding CORS Exception for preflight check

--- src/main/java/com/abj/api_gateway/config/SecurityConfig.java
+++ src/main/java/com/abj/api_gateway/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.abj.api_gateway.config;
 
-import jakarta.ws.rs.HttpMethod;
+import org.springframework.http.HttpMethod;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
